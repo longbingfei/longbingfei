15c8801ea79cbefa9ea9fbe6436bc53576423b74
张显 <zhangxian@hoge.cn>
2016-09-14 14:47:34 +0800

    添加文稿详情页

--- resources/views/admin/article_detail.blade.php
+++ resources/views/admin/article_detail.blade.php
@@ -1,3 +1,8 @@
+<?php
+    if (!isset($detail) || empty($detail) || isset($detail['errorCode'])) {
+        die('数据不存在!');
+    }
+?>
 @extends('admin.home')
 @section('title','文稿详情')
 @section('link')
@@ -8,11 +13,14 @@
 @section('nav')
     @parent
 @stop
-@section('subject','文稿详情')
+@section('subject',$detail['title'].'<a class="a-edit-article" href="'.url('admin/feature/article_form/'.$detail['id']).'">编辑</a>')
 @section('body')
-    <div class="product-detail-container">
-        <div class="image-cut">456</div>
+    <div class="detail-title">
+        <span class="title-sort">分类:&nbsp{{$detail['sort_name']}}</span>
+        <span class="title-pt">发布时间:&nbsp{{$detail['created_at']}}</span>
+        <span class="title-author">发布人:&nbsp{{$detail['author_name']}}</span>
     </div>
-    <script>
-    </script>
+    <div class="article-detail-container">
+        {!! $detail['content'] !!}
+    </div>
 @stop
