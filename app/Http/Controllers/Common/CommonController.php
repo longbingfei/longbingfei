3dbf1884123ba99437f4032342e774dbef5cd463
张显 <zhangxian@hoge.cn>
2016-10-21 13:38:05 +0800

    验证码改为任意字符

--- app/Http/Controllers/Common/CommonController.php
+++ app/Http/Controllers/Common/CommonController.php
@@ -16,7 +16,7 @@ class CommonController extends Controller
 
     public function getVerifyCode()
     {
-        $number = mt_rand(10000, 99999);
+        $number = str_random(5);
         session()->flash('verifycode', $number);
         $image = Image::canvas(90, 40, array(mt_rand(210, 255), mt_rand(210, 255), mt_rand(210, 255)))
             ->text($number, 0, 15, function($font) {
