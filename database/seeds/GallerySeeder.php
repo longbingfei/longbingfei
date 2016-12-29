66e84d3a8c371aa7ed44ab6b5838767f8ca8447d
张显 <zhangxian@hoge.cn>
2016-12-29 09:42:24 +0800

    去除模板多余引用

--- database/seeds/GallerySeeder.php
+++ database/seeds/GallerySeeder.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Database\Seeder;
 use App\Models\Gallery;
+use Illuminate\Database\Seeder;
 
 class GallerySeeder extends Seeder
 {
--- resources/views/tpl/default/gallery_detail.blade.php
+++ resources/views/tpl/default/gallery_detail.blade.php
@@ -4,23 +4,17 @@
     <meta charset="utf-8">
     <title>{{$detail['title']}}</title>
     <link rel="icon" href="{{ url('default/images/site.ico') }}" type="image/x-ico"/>
-    <link href="{{ url('bootstrap/dist/css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ url('default/css/admin_home.css')}}" rel="stylesheet">
     <script src="{{ url('default/js/jquery.1.10.js') }}"></script>
-    <script src="{{ url('bootstrap/dist/js/bootstrap.min.js') }}"></script>
-    <script src="{{ url('default/js/html5shiv.min.js') }}"></script>
     <script src="{{ url('default/js/respond.min.js') }}"></script>
     <script src="{{ url('default/js/main.js') }}"></script>
-    <script src="{{ url('default/js/ready.load.js') }}"></script>
 </head>
 <body>
 <pre>
 <h2>{{$detail['title']}}</h2>
     创建日期:<span>{{$detail['created_at']}}</span>
-
     索引图:
     @if(!empty($detail['index_pic']))<img src="{{url($detail['index_pic']['thumb'])}}">@endif
-
     相册:
     <div id="gallery" style="width:400px;height:300px;"></div>
     <script>
